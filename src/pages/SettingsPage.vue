95ee74a537f6bf08e48353d17dce3ec2787ef4df
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-05-24 02:14:47 +0900

    fix design

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,12 +1,19 @@
 <template>
   <q-page padding>
     <h2 class="text-center">Game Settings</h2>
-    <div class="row justify-center">
-      <div class="col-3 text-center">
-        <q-select v-model="numberOfPlayers" :options="numberOfPlayersOptions" label="Number of players" />
-        <q-select v-model="timeForEachPlayer" :options="timeForEachPlayerOptions" label="Time for each player" />
-        <q-select v-model="startPosition" :options="startPositionOptions" label="Start position" />
-        <q-btn class="q-mt-lg" @click="gameStart" to="/room" unelevated rounded color="primary" label="Game Start" />
+    <div class="text-h6 text-center">
+      プレイ人数・持ち時間・開始位置を選んでください。
+    </div>
+    <div class="row justify-center q-mt-lg">
+      <div class="col-12 col-md-6 text-center">
+        <q-card class="my-card">
+          <q-card-section>
+            <q-select v-model="numberOfPlayers" :options="numberOfPlayersOptions" label="Number of players" />
+            <q-select v-model="timeForEachPlayer" :options="timeForEachPlayerOptions" label="Time for each player" />
+            <q-select v-model="startPosition" :options="startPositionOptions" label="Start position" />
+            <q-btn class="q-mt-lg" @click="gameStart" to="/room" unelevated rounded color="primary" label="Game Start" />
+          </q-card-section>
+        </q-card>
       </div>
     </div>
   </q-page>
