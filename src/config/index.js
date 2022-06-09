31358bdc700acc37371e0a5100b0e54f0c80d576
hayato0309 <hayato.pluto.4538@gmail.com>
2022-06-10 06:42:30 +0900

    fix: game settingsとplayer turnのバグを解消

--- src/config/index.js
+++ src/config/index.js
@@ -1,20 +1,29 @@
 export const Config = {
+    // numberOfPlayers: 2,
+    // numberOfPlayersOptions: [2, 4],
+    // timeForEachPlayers: "10 min",
+    // timeForEachPlayerOptions: ["5 min", "10 min", "20 min"],
+    // timeForEachPlayerObjects:
+    // // { label: "5 min", value: 300 },
+    // // { label: "10 min", value: 600 },
+    // // { label: "20 min", value: 1200 },
+    // {
+    //     "5 min": 300,
+    //     "10 min": 600,
+    //     "20 min": 1200,
+    // },
+    // startPosition: "Corner",
+    // startPositionOptions: ["Center", "Corner", "Anywhere"],
     numberOfPlayers: 2,
-    numberOfPlayersOptions: [2,4],
-    timeForEachPlayers: "10 min",
-    timeForEachPlayerOptions: ["5 min", "10 min", "20 min"],
-    timeForEachPlayerObjects:
-        // { label: "5 min", value: 300 },
-        // { label: "10 min", value: 600 },
-        // { label: "20 min", value: 1200 },
-        {
-            "5 min": 300,
-            "10 min": 600,
-            "20 min": 1200,
-        }
-    ,
-    startPosition:"Corner",
+    numberOfPlayersOptions: [2, 4],
+    timeForEachPlayer: { label: "10 min", value: 600 },
+    timeForEachPlayerOptions: [
+        { label: "5 min", value: 300 },
+        { label: "10 min", value: 600 },
+        { label: "20 min", value: 1200 },
+    ],
+    startPosition: "Corner",
     startPositionOptions: ["Center", "Corner", "Anywhere"],
-}
+};
 
 // { label: "10 min", value: 600 },
